6a73c43ad8042576ac99a3b2bc3c2f5b8ded0253
zhek00 <jonathansantost99@gmail.com>
2026-02-05 15:30:41 -0300

    fix(config): remove incorrect outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
             },
         ],
     },
-    outputFileTracingRoot: path.resolve(__dirname, '../../'),
+
     typescript: {
         ignoreBuildErrors: true,
     },
